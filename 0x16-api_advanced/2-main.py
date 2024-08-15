85229e9802bc46032157d8d911222d2c9972d948
Code-Addict01 <davidckihoto@gmail.com>
2024-08-15 20:04:26 +0300

    Task 4 of api advanced

--- 0x16-api_advanced/2-main.py
+++ 0x16-api_advanced/2-main.py
@@ -4,7 +4,7 @@
 """
 import sys
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     recurse = __import__('2-recurse').recurse
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
--- 0x16-api_advanced/tests/100-main.py
+++ 0x16-api_advanced/tests/100-main.py
@@ -4,7 +4,7 @@
 """
 import sys
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     count_words = __import__('100-count').count_words
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <lis of keywords>".format(sys.argv[0]))
