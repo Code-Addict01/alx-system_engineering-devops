caa75f7061e55a6f0888c70a31aea9511a4ff616
Code-Addict01 <davidckihoto@gmail.com>
2024-08-15 19:48:10 +0300

    Task one of api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
-Defines a function that queries reddit api and
-returns the number of subscribers
+number of subscribers for a given subreddit
 """
+
 from requests import get
+
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
-    
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
